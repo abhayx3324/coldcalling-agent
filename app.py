d9e40032c70f6dfeaeb143f4902d3cef543a3f57
abhayx3324 <1by22cs004@bmsit.in>
2025-01-14 20:54:20 +0530

    Reapply "sdf"
    This reverts commit bccf712f6108b2238cb6066c741a506cab6f7297.

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ def receive_transcript():
     return jsonify({'status': 'success', 'message': 'Transcript received', 'response': ai_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
